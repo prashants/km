2f82fe6de65e915d89cd741c31f23d9a1f76ae79
Prashant Shah <pshah.mumbai@gmail.com>
2012-09-13 17:46:17 +0530

    Create shared memory within the kernel module on mmap
    Signed-off-by: Prashant Shah <pshah.mumbai@gmail.com>

--- mmap/demo_mmap.c
+++ mmap/demo_mmap.c
@@ -21,6 +21,7 @@
 #include <linux/cdev.h>
 #include <linux/semaphore.h>
 #include <linux/mm.h>
+#include <linux/slab.h>
 
 struct demo_mmap_device {
 	int valid;
@@ -31,6 +32,15 @@ struct demo_mmap_device {
 int demo_mmap_major  = 0;
 int demo_mmap_minor  = 1;
 int demo_mmap_nr_dev = 1;
+int demo_mmap_block_size = 0;
+int demo_mmap_part_size = 0;
+
+static void *kmalloc_ptr = NULL;
+static unsigned int *kmalloc_addr = NULL; /* page aligned kmalloc_ptr */
+
+unsigned char counter = 0;
+
+#define N_PAGES	1000
 
 /* Forward declarations */
 static struct vm_operations_struct demo_mmap_vm_ops;
@@ -68,17 +78,31 @@ static int demo_mmap_release(struct inode *inode, struct file *filp)
 
 static int demo_mmap_mmap(struct file *filp, struct vm_area_struct *vma)
 {
+        long length = vma->vm_end - vma->vm_start;
 	printk(KERN_DEBUG "demo_mmap: %s\n", __FUNCTION__);
 
+	/* check the size of shared memory requested */
+	if (length > N_PAGES * PAGE_SIZE) {
+		printk(KERN_NOTICE "demo_mmap: requested memory size exceeded max limits\n");
+		return -EIO;
+	}
+
+	printk(KERN_NOTICE "demo_mmap: vma: start=%lx, pgoff=%lx end=%lx\n",
+			vma->vm_start, vma->vm_pgoff, vma->vm_end);
+
+	vma->vm_flags |= VM_LOCKED;
+
 	/* build page tables to map range of physical memory */
-	if (remap_pfn_range(vma, vma->vm_start, vma->vm_pgoff,
-			vma->vm_end - vma->vm_start,
+	if (remap_pfn_range(vma, vma->vm_start,
+			virt_to_phys((void *)kmalloc_addr) >> PAGE_SHIFT,
+			length,
 			vma->vm_page_prot)) {
+		printk(KERN_NOTICE "demo_mmap: failed to map memory\n");
 		return -EAGAIN;
 	}
 
-	printk(KERN_NOTICE "demo_mmap: vma virt %lx, phys %lx\n",
-		vma->vm_start, vma->vm_pgoff << PAGE_SHIFT);
+	printk(KERN_NOTICE "demo_mmap: vma virt %lx : virt_to_phys %lu\n",
+		vma->vm_start, (unsigned long)(virt_to_phys((void *)kmalloc_addr) >> PAGE_SHIFT));
 
 	vma->vm_ops = &demo_mmap_vm_ops;
 	demo_mmap_vma_open(vma);
@@ -87,7 +111,14 @@ static int demo_mmap_mmap(struct file *filp, struct vm_area_struct *vma)
 
 static void demo_mmap_vma_open(struct vm_area_struct *vma)
 {
+	int c;
 	printk(KERN_DEBUG "demo_mmap: %s\n", __FUNCTION__);
+
+	counter = *(unsigned char *)kmalloc_ptr;
+
+	for (c = 0; c < (PAGE_SIZE * N_PAGES); c++) {
+		*((unsigned char *)kmalloc_ptr + c) = counter;
+	}
 }
 
 static void demo_mmap_vma_close(struct vm_area_struct *vma)
@@ -135,6 +166,27 @@ static int __init demo_mmap_init(void)
 	struct demo_mmap_device *dev = &demo_mmap_dev;	/* local 'dev' pointer to global demo_mmap_dev structure */
 	printk(KERN_DEBUG "demo_mmap: %s\n", __FUNCTION__);
 
+	/*
+	 * allocate kernel shared memory for communication with userspace using mmap
+	 * add 2 extra pages for header and footer, useful if the memory is not
+	 * page aligned
+	 */
+	kmalloc_ptr = kmalloc(PAGE_SIZE * (N_PAGES + 2), GFP_KERNEL);
+	if (kmalloc_ptr) {
+		printk(KERN_INFO "demo_mmap: kmalloc_ptr %p\n", kmalloc_ptr);
+	} else {
+		printk(KERN_INFO "demo_mmap: failed to allocate shared memory\n");
+		return -EIO;
+	}
+
+	/* kmalloc_addr is always page aligned value of kmalloc_ptr */
+	if (((unsigned long)kmalloc_ptr % PAGE_SIZE) == 0) {
+		kmalloc_addr = (int *)kmalloc_ptr;
+	} else {
+		kmalloc_addr = (int *)((unsigned long)kmalloc_ptr +
+			(PAGE_SIZE - ((unsigned long)kmalloc_ptr % PAGE_SIZE)));
+	}
+
 	/* allocating major number for character device */
 	if (demo_mmap_major > 0) {
 		dev_num = MKDEV(demo_mmap_major, demo_mmap_minor);
@@ -147,6 +199,9 @@ static int __init demo_mmap_init(void)
 	if (ret < 0) {
 		demo_mmap_major = 0;
 		printk(KERN_ERR "demo_mmap: failed to allocate major number %d\n", demo_mmap_major);
+		if (kmalloc_ptr) {
+			kfree(kmalloc_ptr);
+		}
 		return ret;
 	}
 	printk(KERN_INFO "demo_mmap: allocated major number %d\n", demo_mmap_major);
@@ -155,6 +210,7 @@ static int __init demo_mmap_init(void)
 	sema_init(&dev->sem, 1);
 	demo_mmap_setup_cdev(dev, 0);
 
+	printk(KERN_INFO "demo_mmap: kmalloc_ptr %p # kmalloc_addr %p\n", kmalloc_ptr, kmalloc_addr);
 	return 0;
 }
 
@@ -171,6 +227,12 @@ static void __exit demo_mmap_exit(void)
 		dev_num = MKDEV(demo_mmap_major, demo_mmap_minor);
 		unregister_chrdev_region(dev_num, demo_mmap_nr_dev);
 	}
+
+	/* free shared memory */
+	if (kmalloc_ptr) {
+		kfree(kmalloc_ptr);
+	}
+
 }
 
 module_init(demo_mmap_init);
