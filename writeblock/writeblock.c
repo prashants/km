0a54a549e4a3844232f15eaeaf8f590e1c3d5ceb
Prashant Shah <pshah.mumbai@gmail.com>
2013-03-05 18:20:51 +0530

    Update write block to use submit_bio()
    Signed-off-by: Prashant Shah <pshah.mumbai@gmail.com>

--- writeblock/writeblock.c
+++ writeblock/writeblock.c
@@ -21,22 +21,15 @@
 #include <linux/buffer_head.h>
 
 #define BLOCK_NUM		137038
+#define SECTOR_NUM		1096304
 
 char *data;
+struct block_device *bd;
+struct file *bd_file;
 
-static int writeblock(char *data)
+static int writebh(char *data)
 {
 	struct buffer_head *bh;
-	struct file *bd_file;
-	struct block_device *bd;
-
-	bd_file = filp_open("/dev/sda1", O_RDONLY, 0600);
-	if (IS_ERR(bd_file)) {
-		printk(KERN_INFO "error opening bd file\n");
-		return -EIO;
-	}
-
-	bd = bd_file->f_dentry->d_inode->i_bdev;
 
 	bh = __bread(bd, BLOCK_NUM, 4096);
 
@@ -56,12 +49,59 @@ static int writeblock(char *data)
 			printk(KERN_ERR "bh_data_ERROR\n");
 		}
 	} else {
-		printk(KERN_ERR "bh_ERROR");
+		printk(KERN_ERR "bh_ERROR\n");
 	}
 
-	filp_close(bd_file, 0);
+	brelse(bh);
+
 
 	return 0;
+}
+
+static int rq_complete(struct bio *b, unsigned int bytes_done, int err)
+{
+        complete((struct completion *)b->bi_private);
+        return 0;
+}
+
+static void writebio(sector_t sect, char *data)
+{
+        struct bio bio;
+        struct bio_vec bio_vec;
+        struct completion complete;
+        size_t size;
+
+	struct page *p = NULL;
+	char *d = NULL;
+
+        size = 4096;
+
+	p = alloc_page(GFP_KERNEL);
+	d = page_address(p);
+	memcpy(d, data, 4096);
+
+        bio_init(&bio);
+        bio.bi_io_vec = &bio_vec;
+        bio_vec.bv_page = p;
+        bio_vec.bv_len = size;
+        bio_vec.bv_offset = 0;
+        bio.bi_vcnt = 1;
+        bio.bi_idx = 0;
+        bio.bi_size = size;
+        bio.bi_bdev = bd;
+        bio.bi_sector = sect;
+        init_completion(&complete);
+        bio.bi_private = &complete;
+        bio.bi_end_io = rq_complete;
+
+        submit_bio(WRITE_SYNC, &bio);
+        wait_for_completion(&complete);
+
+	if (test_bit(BIO_UPTODATE, &bio.bi_flags)) {
+		printk(KERN_INFO "BIO OK\n");
+	} else {
+		printk(KERN_INFO "BIO ERROR\n");
+	}
 }
 
 static int __init writeblock_init(void)
@@ -76,10 +116,19 @@ static int __init writeblock_init(void)
 	}
 
 	for (c = 0; c < 4096; c++) {
-		data[c] = 'A';
+		data[c] = 'B';
 	}
 
-	writeblock(data);
+	bd_file = filp_open("/dev/sda1", O_RDONLY, 0600);
+	if (IS_ERR(bd_file)) {
+		printk(KERN_INFO "error opening bd file\n");
+		return -EIO;
+	}
+
+	bd = bd_file->f_dentry->d_inode->i_bdev;
+
+	//writebh(data);
+	writebio(SECTOR_NUM, data);
 
 	kfree(data);
 
@@ -89,6 +138,8 @@ static int __init writeblock_init(void)
 static void __exit writeblock_exit(void)
 {
 	printk(KERN_INFO "list: %s\n", __FUNCTION__);
+
+	filp_close(bd_file, 0);
 }
 
 module_init(writeblock_init);
