60efa7c2ef4cc8a233fdcff92f2cf3d972d9dbe3
Prashant Shah <pshah.mumbai@gmail.com>
2013-06-26 15:00:03 +0530

    Add kernel parameters demo
    Signed-off-by: Prashant Shah <pshah.mumbai@gmail.com>

---
+++ parameter/parameter.c
@@ -0,0 +1,58 @@
+/*
+ * parameter - module parameters demo
+ *
+ * Written in 2012 by Prashant P Shah <pshah.mumbai@gmail.com>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining
+ * a copy of this software and associated documentation files (the
+ * "Software"), to deal in the Software without restriction, including
+ * without limitation the rights to use, copy, modify, merge, publish,
+ * distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to
+ * the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+ * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+ * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+ * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE
+ */
+
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/interrupt.h>
+#include <linux/slab.h>
+
+int age = 0;
+module_param(age, int, 0);
+MODULE_PARM_DESC(age, "Your age");
+
+char *name = "NULL";
+module_param(name, charp, 0000);
+
+static int __init param_init(void)
+{
+	printk(KERN_INFO "param: %s\n", __FUNCTION__);
+
+	printk(KERN_INFO "name=%s age=%d\n", name, age);
+	return 0;
+}
+
+static void __exit param_exit(void)
+{
+	printk(KERN_INFO "param: %s\n", __FUNCTION__);
+
+}
+
+module_init(param_init);
+module_exit(param_exit);
+
+MODULE_LICENSE("Dual MIT/GPL");
+MODULE_AUTHOR("Prashant Shah<pshah.mumbai@gmail.com>");
+
